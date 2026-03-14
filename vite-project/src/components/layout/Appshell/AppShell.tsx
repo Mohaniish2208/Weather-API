1bdb6f5a5425d9fd672e95a51cbb30025ebebe53
Mohaniish2208 <mohaniish5@gmail.com>
2026-03-14 17:13:20 -0400

    Appshell.tsx file updated.

--- vite-project/src/components/layout/Appshell/AppShell.tsx
+++ vite-project/src/components/layout/Appshell/AppShell.tsx
@@ -1,3 +1,20 @@
+import SearchForm from "../../weather/SearchForm/SearchForm"
+import StatusMessage from "../../weather/StatusMessage/StatusMessage"
+import WeatherCard from "../../weather/WeatherCard/WeatherCard"
+
 export default function Appshell() {
-  return <></>
+  return (
+    <main className="appshell">
+      <div className="appshell-container">
+        <header className="appshell-container-header">
+          <h1>Weather API Dashboard</h1>
+          <p>Search any city to view weather details.</p>
+        </header>
+
+        <SearchForm />
+        <StatusMessage message="Search for a city to begin." />
+        <WeatherCard />
+      </div>
+    </main>
+  )
 }
