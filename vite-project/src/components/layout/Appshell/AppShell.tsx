7da1ccecd73c5cc5dc23e1d574a30fd202a96086
Mohaniish2208 <mohaniish5@gmail.com>
2026-03-18 03:20:36 -0400

    AppShell.tsx updated.

--- vite-project/src/components/layout/Appshell/AppShell.tsx
+++ vite-project/src/components/layout/Appshell/AppShell.tsx
@@ -2,8 +2,22 @@ import SearchForm from "../../weather/SearchForm/SearchForm"
 import StatusMessage from "../../weather/StatusMessage/StatusMessage"
 import WeatherCard from "../../weather/WeatherCard/WeatherCard"
 import "../../../styles/AppShell.css"
+import { useState } from "react"
+
+type WeatherState = {
+  cityNAme: string
+  temperature: number
+  humidity: number
+  windSpeed: number
+  condition: string
+}
 
 export default function Appshell() {
+  const [city, setCity] = useState("")
+  const [statusMessage, setStatusMessage] = useState("Search for a city to begin")
+  const [weather, setWeather] = useState<WeatherState | null>(null)
+  const [loading, setLoading] = useState(false)
+
   return (
     <main className="appshell">
       <div className="appshell-container">
